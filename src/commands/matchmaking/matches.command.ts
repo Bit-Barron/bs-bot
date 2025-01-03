08cd2dbe1040038061f0535cc97c148a79e893ed
Bit-Barron <bit.barron22@gmail.com>
2025-01-03 20:34:33 +0100

    easy

--- src/commands/matchmaking/matches.command.ts
+++ src/commands/matchmaking/matches.command.ts
@@ -1,6 +1,8 @@
 import { Discord, Slash } from "discordx";
 import prisma from "../../utils/prisma";
 import { MatchType } from "../../types/matchmaking/match.types";
+import { CommandInteraction } from "discord.js";
+import { createEmbed } from "../../helpers/discord.helper";
 
 @Discord()
 export class MatchesCommand {
@@ -8,12 +10,29 @@ export class MatchesCommand {
     name: "matches",
     description: "Get all matches",
   })
-  async matches(): Promise<MatchType[]> {
+  async matches(interaction: CommandInteraction): Promise<MatchType[]> {
     const getMatches = await prisma.match.findMany();
 
-    return getMatches.map((match) => ({
+    const matches = getMatches.map((match) => ({
       ...match,
       status: match.status as MatchType["status"],
     }));
+
+    await interaction.editReply({
+      embeds: [
+        createEmbed(
+          "Matches",
+          matches
+            .map(
+              (match) =>
+                `ID: ${match.id}, Team 1: ${match.team1}, Team 2: ${match.team2}, Status: ${match.status}, Created At: ${match.createdAt}`,
+            )
+            .join("\n"),
+          "Green",
+        ),
+      ],
+    });
+
+    return matches;
   }
 }
